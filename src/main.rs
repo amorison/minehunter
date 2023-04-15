637646b8f6c39c882ee6755763d61015f031e185
Adrien Morison <adrien.morison@gmail.com>
2023-04-15 19:54:27 +0100

    Do not maximize Windows on startup
    This doesn't work everywhere.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,9 @@
 use minehunter::MineHunterApp;
 
 fn main() -> Result<(), eframe::Error> {
-    let native_options = eframe::NativeOptions {
-        maximized: true,
-        ..Default::default()
-    };
     eframe::run_native(
         "Mine Hunter",
-        native_options,
+        eframe::NativeOptions::default(),
         Box::new(|cc| Box::new(MineHunterApp::new(cc))),
     )
 }
