367f6f22a976d8f958fe6db2538890303ecce62a
Adrien Morison <adrien.morison@gmail.com>
2023-04-16 17:50:19 +0100

    Larger initial window size

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
+use eframe::epaint::vec2;
 use minehunter::MineHunterApp;
 
 fn main() -> Result<(), eframe::Error> {
+    let opts = eframe::NativeOptions {
+        initial_window_size: Some(vec2(1080.0, 720.0)),
+        ..Default::default()
+    };
     eframe::run_native(
         "Mine Hunter",
-        eframe::NativeOptions::default(),
+        opts,
         Box::new(|cc| Box::new(MineHunterApp::new(cc))),
     )
 }
